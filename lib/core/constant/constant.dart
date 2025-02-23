6bd843ce9af12bedcb64a50bd5dcbbbb5d7b9772
amirhossein baghi <dev.amir.b22@gmail.com>
2025-02-23 14:35:19 +0330

    constant and color

---
+++ lib/core/constant/constant.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static const baseUrl = 'https://Dunijet.ir/content/projects/hotelino/';
+  static const hotelsData = "assets/data/hotels.json";
+}
+
+class Appcolors {
+  static const Color primary = Color(0xffb27258);
+
+  //light theme color
+  static const Color lightText = Colors.black;
+  static const Color lightHint = Colors.grey;
+  static const Color lightInputFill = Color(0xfff5f5f5);
+  static const Color lightBorder = Colors.grey;
+  static const Color lightFocusBorder = Colors.brown;
+
+  //dark theme
+  static const Color darkText = Colors.white;
+  static const Color darkHint = Colors.grey;
+  static const Color karkInputFill = Color(0xfff5f5f5);
+  static const Color darkBorder = Color(0xff707070);
+  static const Color darkFocusBorder = Colors.brown;
+  static const Color darkButton = Color(0xff5d4037);
+}

055545eac3a65df44f64a174e4846000a72f3cd4
amirhossein baghi <dev.amir.b22@gmail.com>
2025-03-05 11:41:45 +0330

    onboarding completed

--- lib/feachures/onboarding/presentation/onboarding_page.dart
+++ lib/feachures/onboarding/presentation/onboarding_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hotelino/feachures/onboarding/presentation/onboarding_provider.dart';
+import 'package:hotelino/feachures/onboarding/presentation/widgets/onboarding_button.dart';
 import 'package:hotelino/feachures/onboarding/presentation/widgets/onboarding_items.dart';
+import 'package:hotelino/routes/app_route.dart';
 import 'package:provider/provider.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -39,8 +41,67 @@ class _OnboardingPageState extends State<OnboardingPage> {
           buildPageIndicator(
               onboardingprovider.currentPage, totalPage, context),
           SizedBox(
-            height: 80,
-          )
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                OnboardingButton(
+                    visible: onboardingprovider.currentPage > 0,
+                    icon: Icons.arrow_back,
+                    backgroundcolor: Colors.transparent,
+                    iconColor: Theme.of(context).colorScheme.primary,
+                    onPeressed: () {
+                      if (onboardingprovider.currentPage > 0) {
+                        _pageControler.previousPage(
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.ease);
+                      }
+                    }),
+                OnboardingButton(
+                    visible: onboardingprovider.currentPage < totalPage - 1,
+                    icon: Icons.arrow_forward,
+                    backgroundcolor: Theme.of(context).colorScheme.primary,
+                    iconColor: Colors.white,
+                    onPeressed: () {
+                      if (onboardingprovider.currentPage < totalPage - 1) {
+                        _pageControler.nextPage(
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.ease);
+                      }
+                    })
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          if (totalPage > 1) ...[
+            AnimatedSwitcher(
+                duration: Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return SizeTransition(
+                      sizeFactor: animation, axisAlignment: -1, child: child);
+                },
+                child: onboardingprovider.currentPage == totalPage - 1
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, AppRoute.home);
+                              },
+                              child: Text("شروع رزرو هتل ها")),
+                        ),
+                      )
+                    : null)
+          ]
         ],
       ),
     );
---
+++ lib/feachures/onboarding/presentation/widgets/onboarding_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class OnboardingButton extends StatelessWidget {
+  final bool visible;
+  final IconData icon;
+  final Color backgroundcolor;
+  final Color iconColor;
+  final VoidCallback onPeressed;
+
+  const OnboardingButton(
+      {super.key,
+      required this.visible,
+      required this.icon,
+      required this.backgroundcolor,
+      required this.iconColor,
+      required this.onPeressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 300),
+      transitionBuilder: (child, animation) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+      child: visible
+          ? Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                      color: Theme.of(context).colorScheme.primary, width: 1)),
+              child: FloatingActionButton(
+                onPressed: onPeressed,
+                shape: CircleBorder(),
+                backgroundColor: backgroundcolor,
+                elevation: 0,
+                child: Icon(
+                  icon,
+                  color: iconColor,
+                ),
+              ),
+            )
+          : const SizedBox(),
+    );
+  }
+}

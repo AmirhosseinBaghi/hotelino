792e1f962afe8b1119a191ef6127d4fdf967ce8f
amirhossein baghi <dev.amir.b22@gmail.com>
2025-04-07 13:20:14 +0330

    add hotel card file

---
+++ lib/feachures/home/presentation/widgets/hotel_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hotelino/feachures/home/data/models/model.dart';
+
+class HotelCard extends StatelessWidget {
+  Hotel hotel;
+
+  HotelCard({super.key, required this.hotel});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}

ee7c1adab1a5b6aa39c087992b3a1f45538cbda8
amirhossein baghi <dev.amir.b22@gmail.com>
2025-04-14 00:12:46 +0330

    add favorite hotel list

--- lib/feachures/favorite/presentation/favorite_page.dart
+++ lib/feachures/favorite/presentation/favorite_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/feachures/favorite/presentation/widget/favorite_item.dart';
+import 'package:hotelino/feachures/home/presentation/provider/favorite_list_provider.dart';
+import 'package:hotelino/feachures/home/presentation/widgets/searchbar.dart';
+import 'package:provider/provider.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "هتل های مورد علاقه",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            SearchbarWidget(),
+            SizedBox(
+              height: 16,
+            ),
+            Consumer<FavoriteListProvider>(
+              builder: (context, favoriteProvider, child) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                  child: FavoriteHotelCard(
+                    hotel: favoriteProvider.favoritesHotelList[0],
+                    onRemoveFavotiteClicked: (hotelId) {
+                      favoriteProvider.toggleFavorite(hotelId);
+                    },
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

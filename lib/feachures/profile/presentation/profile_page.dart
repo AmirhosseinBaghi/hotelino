1f62f265f86c1fb6949ae5f5e5a4ce0d7034c07a
amirhossein baghi <dev.amir.b22@gmail.com>
2025-05-23 15:41:49 +0330

    add ImagePicker function and profile avatar UI

--- lib/feachures/profile/presentation/profile_page.dart
+++ lib/feachures/profile/presentation/profile_page.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hotelino/feachures/home/presentation/provider/profile_provider.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,8 +13,82 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  File? _image;
+
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+
+    if (picked != null) {
+      setState(() {
+        _image = File(picked.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ProfileProvider>(
+      builder: (context, profileProvider, child) {
+        final profile = profileProvider.profile;
+
+        if (profile == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return Scaffold(
+          body: SafeArea(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundImage: _image != null
+                          ? FileImage(_image!)
+                          : NetworkImage(profile.avatarUrl) as ImageProvider,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: _getImage,
+                        child: Container(
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primaryFixed,
+                              shape: BoxShape.circle),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  profile.name,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  profile.email,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Colors.grey),
+                )
+              ],
+            ),
+          )),
+        );
+      },
+    );
   }
 }
